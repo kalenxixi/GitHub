0bf3d9b2e51cb99a72f798441241182b17b7369c
kalenxixi <chen.kailong@outlook.com>
2016-03-13 21:20:09 -0700

    Roll-a-ball tutorial
    Add the force to a ball and then you can use keyboard to roll the ball

---
+++ Unity3D/Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement);
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
